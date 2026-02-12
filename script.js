1e45a6371e74b609272e27430db90f1529fe07f0
Timer110 <121953691+Timer110@users.noreply.github.com>
2026-02-12 11:20:10 +0100

    reworked whole Script, whole code made private

--- script.js
+++ script.js
@@ -1,106 +1,115 @@
-//HTML Variables
-const currentPlayer = document.querySelector(".currentPlayer");
-const display = document.querySelector(".display");
-const resetButton = document.querySelector(".resetButton");
+const Gameboard = (function () {
+    const board = Array(9).fill("");
+    
+    const getBoard = () => board;
 
-//JS Variables
+    const setField = (index, marker) => {
+        if (board[index] !== "") return false;
+        board [index] = marker;
+        return true;
+    };
+    
+    const reset = () => {
+        board.fill("");
+    };
 
-let Players = {};
-Players.player1 = new Player("Tobias", "X");
-Players.player2 = new Player("Markus", "O");
-Players.activePlayer = false; //false = Player1, true = Player2
+    return {getBoard, setField, reset};
+})();
 
-let gameBoard = Array(9).fill("");
-
-//Player-Constructor
-
-function Player(name, marker) {
-    this.name = name;
-    this.marker = marker;
+const Player = (name, marker) => {
+    return {name, marker};
 };
 
+const GameController = (function () {
+    
+    const player1 = Player("Tobias", "X");
+    const player2 = Player("Markus", "O");
+    let activePlayer = player1;
 
-//Functions
+    const switchPlayer = () => {
+        activePlayer = activePlayer === player1 ? player2 : player1;
+    };
 
-function createGameBoard() {
-    gameBoard.forEach((_, index) => {
-        let field = document.createElement("div");
-        field.setAttribute("class", "boardField");
-        field.setAttribute("id", index);
-        display.appendChild(field);
-    });
-}
+    const getActivePlayer = () => activePlayer;
 
-function setMarker(fieldID) {
-    const field = document.getElementById(fieldID);
-    if (!Players.activePlayer && field.textContent == "") { //Wenn Player 1 aktiv ist
-        gameBoard[fieldID] = Players.player1.marker;
-        if (checkWinner()) return;
-        playerChange();
-    }
-    else if (Players.activePlayer && field.textContent == "") { //Wenn Player 2 aktiv ist
-        gameBoard[fieldID] = Players.player2.marker;
-        if (checkWinner()) return;
-        playerChange();
-    }
-}
+    const playRound = (index) => {
+        if (!Gameboard.setField(index, activePlayer.marker)) return;
 
-function resetGameBoard() {
-    gameBoard.fill("");
-    updateGameBoard();
-}
-
-function playerChange() {
-    Players.activePlayer = !Players.activePlayer;
-    currentPlayer.textContent = Players.activePlayer ? (Players.player2.name + " ist am Zug!") : (Players.player1.name + " ist am Zug!");
-}
-
-function updateGameBoard() {
-    gameBoard.forEach((element, index) => {
-        document.getElementById(index).textContent = element;
-    });
-}
-
-function checkWinner(){
-    const winPatterns = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontal
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], //vertikal
-        [0, 4, 8], [2, 4, 6]            //diagonal
-    ];
-
-    for (let pattern of winPatterns) {
-        const [a, b, c] = pattern;
-        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-            alert("Gewonnen!");
-            return true;
+        if (checkWinner()) {
+            alert(`${activePlayer.name} hat gewonnen!`);
+            return;
         }
-    }
-    return false;
-} 
 
-//Eventlistener
+        switchPlayer();
+    };
 
-display.addEventListener("click", (e) => {
-    if (e.target.classList.contains("boardField")) {
-        setMarker(e.target.id);
-        updateGameBoard();
-    }
-})
+    const checkWinner = () => {
+        const board = Gameboard.getBoard();
 
-resetButton.addEventListener("click", () => {
-    resetGameBoard();
-});
-//
+        const winPatterns = [
+            [0,1,2],[3,4,5],[6,7,8],
+            [0,3,6],[1,4,7],[2,5,8],
+            [0,4,8],[2,4,6]
+        ]
+
+        return winPatterns.some(([a, b, c]) => 
+            board[a] &&
+            board[a] === board[b] &&
+            board[a] === board[c]
+
+        );
+    };
+
+    const resetGame = () => {
+        Gameboard.reset();
+        activePlayer = player1;
+    };
+
+    return {playRound, getActivePlayer, resetGame};
+})();
+
+const DisplayController = (function() {
+
+    const display = document.querySelector(".display");
+    const currentPlayerText = document.querySelector(".currentPlayer");
+    const resetButton = document.querySelector(".resetButton");
+
+    const render = () => {
+        display.innerHTML = "";
+        const board = Gameboard.getBoard();
+
+        board.forEach((value, index) => {
+            const field = document.createElement("div");
+            field.classList.add("boardField");
+            field.textContent = value;
+            field.addEventListener("click", () => {
+                GameController.playRound(index);
+                render();
+                updateCurrentPlayer();
+            });
+
+            display.appendChild(field);
+        });
+    };
+    
+    const updateCurrentPlayer = () => {
+        currentPlayerText.textContent = `${GameController.getActivePlayer().name} ist am Zug!`;
+    };
+
+    resetButton.addEventListener("click", () => {
+        GameController.resetGame();
+        render();
+        updateCurrentPlayer();
+    });
+
+    const init = () => {
+        render();
+        updateCurrentPlayer();
+    };
+
+    return {init};
+
+})();
 
 
-//Initialisation
-currentPlayer.textContent = Players.player1.name + " ist am Zug!"
-
-
-
-createGameBoard();
-resetGameBoard();
-
-//ToDo
-
-//Logik mit pattern machen, siehe ChatGPT
+DisplayController.init();
